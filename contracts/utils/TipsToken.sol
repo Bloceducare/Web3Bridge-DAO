83f93ea653b07a9f229da12ef27e1bd5a43eb799
CodingCas <casweeno2000@gmail.com>
2022-11-02 23:30:51 +0100

    [CHORE]-Casweeney: created TipsTokenFactoryFacet

--- contracts/utils/TipsToken.sol
+++ contracts/utils/TipsToken.sol
@@ -1,7 +1,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-
 contract TipsToken {
-    
+    constructor(
+        string memory _tokenName,
+        string memory _tokenSymbol,
+        uint8 _tokenDecimal,
+        uint256 tokenSupply
+    ) {}
 }
