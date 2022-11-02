4a4f519e350a3901d9e7c1729956e720cc32e71b
developeruche <nweleuchenna2004@gmail.com>
2022-11-02 17:42:24 +0100

    developeruche: started working on the certificate factory facet

--- contracts/facets/CertificateFactoryFacet.sol
+++ contracts/facets/CertificateFactoryFacet.sol
@@ -2,6 +2,13 @@
 pragma solidity ^0.8.0;
 
 
+
+/// @dev this contract would be deploying the cerificate of the cohort after cohort ends 
+/// @notice only person would has payed their $1500 would be able to mint from the deployed nft 
 contract CertificateFactoryFacet {
-    
+    /// @notice this function would be handling the deployment 
+    /// @dev only the admin of the diamond would be able to call this function 
+    function depolyCertificate(string memory name, string memory symbol) external {
+
+    }
 }
