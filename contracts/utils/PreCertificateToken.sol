0e865d59e7c015a69cea6216510aaf25a1bb98d5
kayzee <olaniyikazeem112@gmail.com>
2022-11-06 13:03:24 +0100

    completed preCert

--- contracts/utils/PreCertificateToken.sol
+++ contracts/utils/PreCertificateToken.sol
@@ -2,6 +2,10 @@
 pragma solidity ^0.8.0;
 
 import "../interfaces/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
+
+
 
 
 
@@ -12,16 +16,32 @@ import "../interfaces/IERC20.sol";
 /// The mode of payment can either be in USDT or MATIC
 /// The admin will set the fee to be paid by students
 
-contract PreCertificateToken {
+contract PreCertificateToken is ERC20("Pre-Certificate Token", "WPC") {
     // Custom errors
     error notAdmin(string);
+
+    /*   Events */
+    
 
     /*   State variable   */
     bytes32 public merkleRoot;
     IERC20 public USDTContractAddr;
-    uint40 public cohortFee;
+    uint256 public cohortFee;
     address public admin;
+    uint40 paymentStart;
+    uint40 elapsedTime;
+    uint40 additionalTime;
 
+
+    struct StudentDetails{
+        uint256 amountPaid;
+        bool claimed;
+        uint8 tokenRecieved;
+        uint40 timeOfLastPayment;
+
+    }
+
+    mapping(address => StudentDetails) studentDetails;
 
 
     constructor(address _admin){
@@ -29,15 +49,61 @@ contract PreCertificateToken {
     }
 
 /// @notice this function can only be called by the admin
-    function setFee(uint40 _amount, bytes32 _merkleRoot, IERC20 _USDTContractAddr) public {
+    function setFee(uint40 _amount, bytes32 _merkleRoot, IERC20 _USDTContractAddr, uint40 _elapsedTime, uint40 _additionalTime) public {
         if(msg.sender == admin){
             cohortFee = _amount;
             merkleRoot = _merkleRoot;
             USDTContractAddr = _USDTContractAddr;
+            paymentStart = block.timestamp;
+            elapsedTime = block.timestamp + (_elapsedTime * 4 weeks);
+            additionalTime = block.timestamp + (_additionalTime * 4 weeks);
+
         }
         else{
             revert notAdmin("Not an Admin");
         }
+    }
+/// @dev A function for student's payment
+/// @notice A seperate function is created for fee payment in other allow installmental payment
+    function payFee(uint40 _stableAmount, bytes32 memory _merkleProof) public {
+        StudentDetails storage sd = studentDetails[msg.sender];
+        bytes32 leaf = keccak256(msg.sender);
+        if(MerkleProof.verify(_merkleProof, merkleRoot, leaf) && block.timestamp < additionalTime){
+            // assert(_stableAmount >= (cohortFee * 70)/100) ;
+            USDTContractAddr.transferFrom(msg.sender, address(this), _stableAmount);
+            sd.amountPaid += _stableAmount;
+            sd.timeOfLastPayment = block.timestamp;
+        }
+        else if(block.timestamp > additionalTime && sd.amountPaid < cohortFee){
+            uint256 amount = sd.amountPaid;
+            sd.amountPaid = 0;
+            USDTContractAddr.transfer(msg.sender, amount);
+        }
+    }
+
+    function checkCompleted() public returns(bool) {
+        StudentDetails memory sd = studentDetails[msg.sender];
+        if(sd.amountPaid == cohortFee){
+            return true;
+        }
+    }
+
+    function claimToken(bytes32 memory _merkleProof) public {
+        StudentDetails memory sd = studentDetails[msg.sender];
+        bytes32 leaf = keccak256(msg.sender);
+        if(MerkleProof.verify(_merkleProof, merkleRoot, leaf)){
+            assert(checkCompleted());
+            assert(!sd.claimed);
+            if(block.timestamp < elapsedTime){
+                _mint(msg.sender, 2);
+            }
+            else if(block.timestamp > elapsedTime){
+                _mint(msg.sender, 1);
+            }
+        }
+
+        sd.claimed = true;
+
     }
 
 
