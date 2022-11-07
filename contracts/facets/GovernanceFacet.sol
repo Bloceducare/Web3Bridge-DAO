c0f79f7ae0fa1da25a488cb25e3f2a6adeae5c93
CodingCas <casweeno2000@gmail.com>
2022-11-07 20:12:40 +0100

    [CHORE]-Casweeney: GovernanceFacet completed

--- contracts/facets/GovernanceFacet.sol
+++ contracts/facets/GovernanceFacet.sol
@@ -4,9 +4,13 @@ pragma solidity ^0.8.0;
 import "../libraries/LibAppStorage.sol";
 import "../interfaces/IDAOToken.sol";
 
+/// @dev this contract handles everything about Governance which entails
+/// creating proposals by the admin, voting for proposals by the DAO members
 contract GovernanceFacet {
     States internal states;
 
+    /// @dev this function creates a proposal
+    /// only owner of the contract can create proposals
     function createProposal(string memory _name, uint256 _endTime) external {
         require(msg.sender == states.owner, "only owner");
         require(_endTime > block.timestamp, "invalid end time");
@@ -19,12 +23,15 @@ contract GovernanceFacet {
         states.proposalCount += 1;
     }
 
+    /// @dev this function is used to cancel a proposal, given a proposalID
+    /// only owner of the contract can create proposals
     function cancelProposal(uint256 _proposalID) external {
         require(msg.sender == states.owner, "only owner");
         require(states.proposals[_proposalID].cancelled == false, "proposal already cancelled");
         states.proposals[_proposalID].cancelled = true;
     }
 
+    /// @dev this function returns all created proposals
     function getProposals() public view returns (Proposal[] memory) {
         uint256 currentProposalIndex = 0;
         Proposal[] memory allProposals = new Proposal[](states.proposalCount);
@@ -39,13 +46,29 @@ contract GovernanceFacet {
         return allProposals;
     }
 
+    /// @dev this function returns the count(number) of total proposals created
+    function proposalCount() external view returns (uint256) {
+        return states.proposalCount;
+    }
+
+    /// @dev this function is used to vote for a proposal, given the proposalID
+    /// it takes in a _voteType which can be 1 or 0 (one or zero):
+    /// 1 is for a support vote, 0 is for an against vote
+    /// it takes in a voteWeight which is equivalent to the number of votes a user is given
+    /// users can only vote with a DAO token, and this token is burnt when user votes.
+    /// the number of token burrnt is equivalent to the voteWeight a user used for voting.
+    /// User can only vote once
     function voteProposal(
         uint256 _proposalID,
         uint256 _voteType,
         uint256 _voteWeight
     ) external {
-        require(IDAOToken(states.daoToken).balanceOf(msg.sender) >= _voteWeight, "insufficient token to vote");
+        require(states.voted[msg.sender][_proposalID] == false, "already voted");
+        require(IDAOToken(states.daoToken).balanceOf(msg.sender) >= _voteWeight, "insufficient DAO token to vote");
         require(states.proposals[_proposalID].cancelled == false, "proposal is cancelled");
+
+        states.voteID = states.voteID + 1;
+
         IDAOToken(states.daoToken).burn(msg.sender, _voteWeight);
 
         if (_voteType == 1) {
@@ -57,12 +80,40 @@ contract GovernanceFacet {
         }
 
         states.proposals[_proposalID].voteCount += _voteWeight;
+        states.proposals[_proposalID].votersCount += 1;
         states.voted[msg.sender][_proposalID] = true;
+
+        Vote storage newVote = states.votes[states.voteID];
+        newVote.proposalID = _proposalID;
+        newVote.voter = msg.sender;
+        newVote.weight = _voteWeight;
+        states.totalVoteCount += 1;
     }
 
-    function proposalCount() external view returns (uint256) {
-        return states.proposalCount;
-    }
+    function getVoters(uint256 _proposalID) public view returns (Vote[] memory) {
+        uint256 currentVoterIndex = 0;
+        uint256 votesCount = states.totalVoteCount;
+        uint256 proposalVotersCount = 0;
 
-    function getVotes() public view returns (uint256[] memory) {}
+        for (uint256 i = 0; i < votesCount; i++) {
+            if (states.votes[i + 1].proposalID == _proposalID) {
+                proposalVotersCount += 1;
+            }
+        }
+
+        Vote[] memory allVoters = new Vote[](proposalVotersCount);
+
+        for (uint256 i = 0; i < votesCount; i++) {
+            if (states.votes[i + 1].proposalID == _proposalID) {
+                uint256 currentID = i + 1;
+
+                Vote storage currentVoter = states.votes[currentID];
+
+                allVoters[currentVoterIndex] = currentVoter;
+                currentVoterIndex += 1;
+            }
+        }
+
+        return allVoters;
+    }
 }
--- contracts/libraries/LibAppStorage.sol
+++ contracts/libraries/LibAppStorage.sol
@@ -5,24 +5,27 @@ struct Proposal {
     uint256 id;
     string name;
     uint256 endTime;
+    uint256 votersCount;
     uint256 voteCount;
     uint256 support;
     uint256 against;
     bool cancelled;
 }
 
-struct Voter {
+struct Vote {
+    uint256 proposalID;
+    address voter;
     uint256 weight;
-    bool voted;
-    uint256 proposalVoted;
 }
 
 struct States {
     address owner;
     mapping(uint256 => Proposal) proposals;
     mapping(address => mapping(uint256 => bool)) voted;
-    mapping(uint256 => Voter) proposalVoters;
+    mapping(uint256 => Vote) votes;
     uint256 ID;
+    uint256 voteID;
     uint256 proposalCount;
+    uint256 totalVoteCount;
     address daoToken;
 }
