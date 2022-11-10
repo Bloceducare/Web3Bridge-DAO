e0664674026737e49981e753fe96f7f8854ec237
developeruche <nweleuchenna2004@gmail.com>
2022-11-10 19:57:41 +0100

    developeruche: working of the dao admin ops facet

--- contracts/facets/AdminOPsFacet.sol
+++ contracts/facets/AdminOPsFacet.sol
@@ -3,5 +3,8 @@ pragma solidity ^0.8.0;
 
 
 contract AdminOpsFacet {
-    
+    /// @notice this function can be called by the admin to force pay even after the deadline 
+    function force_pay() external {
+
+    }
 }
