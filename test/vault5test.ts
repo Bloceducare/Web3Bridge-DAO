ede65258413368c6d836900417fd2ac5487683c4
Ultra-tech-code <hizick27@gmail.com>
2022-11-21 08:38:35 +0100

    resolved vault5 test issue

--- test/vault5test.ts
+++ test/vault5test.ts
@@ -8,60 +8,60 @@ import { it } from "mocha";
 
 describe("Vault5", function () {
   async function deploysVaultAndToken() {
-    const [owner, student1, student2] = await ethers.getSigners();
+    const [admin, student1, student2] = await ethers.getSigners();
 
     const Token = await ethers.getContractFactory("VaultToken");
     const token = await Token.deploy("Tether", "USDT");
 
     const Vault5 = await ethers.getContractFactory("Vault5");
-    const vault5 = await Vault5.deploy(token.address, owner.address);
+    const vault5 = await Vault5.deploy(token.address, admin.address);
 
 
-    return { owner, student1, student2,  token, vault5 };
+    return { admin, student1, student2,  token, vault5 };
   }
 
 
   
   describe("Vault 5 Test", function () {
-    it("Ensure the owner have deposited in vault contract", async function () {
-      const {  owner, student1, student2,  token, vault5} = await loadFixture(
+    it("Ensure the admin have deposited in vault contract", async function () {
+      const {  admin, student1, student2,  token, vault5} = await loadFixture(
         deploysVaultAndToken
       );
      const depositValue = ethers.utils.parseEther("20000")
-      await token.connect(owner).mint(owner.address, ethers.utils.parseEther("200000"))
-      const balanceOfOwner = await token.connect(owner).balanceOf(owner.address)
-      console.log("Owner balance is", balanceOfOwner.toString())
-      await token.connect(owner).approve(vault5.address, ethers.utils.parseEther("100000000000"))
-      await vault5.connect(owner).depositIntoVault(depositValue)
+      await token.connect(admin).mint(admin.address, ethers.utils.parseEther("200000"))
+      const balanceOfadmin = await token.connect(admin).balanceOf(admin.address)
+      console.log("admin balance is", balanceOfadmin.toString())
+      await token.connect(admin).approve(vault5.address, ethers.utils.parseEther("100000000000"))
+      await vault5.connect(admin).depositIntoVault(depositValue)
       expect(await vault5.returnVaultBalace()).to.equal(depositValue);
     });
     // Ensure that student cannot withdraw until the Vault is open
 
     it("Check if vault is open", async function () {
-        const {  owner, student1, student2,  token, vault5} = await loadFixture(
+        const {  admin, student1, student2,  token, vault5} = await loadFixture(
             deploysVaultAndToken
           );
 
       const depositValue = ethers.utils.parseEther("20000")
-      await token.mint(owner.address, ethers.utils.parseEther("200000"))
-      await token.connect(owner).approve(vault5.address, ethers.utils.parseEther("100000000000"))
-      await vault5.connect(owner).depositIntoVault(depositValue)
-      await vault5.connect(owner).openVault()
+      await token.mint(admin.address, ethers.utils.parseEther("200000"))
+      await token.connect(admin).approve(vault5.address, ethers.utils.parseEther("100000000000"))
+      await vault5.connect(admin).depositIntoVault(depositValue)
+      await vault5.connect(admin).openVault()
       
       expect(await vault5.checkIfWithdrawTimeReached()).to.equal(true)     
 
     });
 
     it("Ensure that student can withdraw just their share  ", async function () {
-      const {  owner, student1, student2,  token, vault5} = await loadFixture(
+      const {  admin, student1, student2,  token, vault5} = await loadFixture(
         deploysVaultAndToken
       );
 
       const depositValue = ethers.utils.parseEther("20000")
-      await token.mint(owner.address, ethers.utils.parseEther("200000"))
-      await token.connect(owner).approve(vault5.address, ethers.utils.parseEther("100000000000"))
-      await vault5.connect(owner).depositIntoVault(depositValue)
-      await vault5.connect(owner).openVault()
+      await token.mint(admin.address, ethers.utils.parseEther("200000"))
+      await token.connect(admin).approve(vault5.address, ethers.utils.parseEther("100000000000"))
+      await vault5.connect(admin).depositIntoVault(depositValue)
+      await vault5.connect(admin).openVault()
       await vault5.connect(student1).addAddressOfEarlyPayment()
       await vault5.connect(student2).addAddressOfEarlyPayment()
       await vault5.connect(student2).withdrawShare()
@@ -76,11 +76,11 @@ describe("Vault5", function () {
   });
 
 
-  it("Test to ensure that only Owner can open vault", async function () {
-    const {  owner, student1, student2,  token, vault5} = await loadFixture(
+  it("Test to ensure that only admin can open vault", async function () {
+    const {  admin, student1, student2,  token, vault5} = await loadFixture(
         deploysVaultAndToken
       );
-  await vault5.connect(owner).openVault()
+  await vault5.connect(admin).openVault()
   expect(await vault5.checkIfWithdrawTimeReached()).to.equal(true)     
 
 });
