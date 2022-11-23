e029a9414d04560291722e3257b8b4c9f940574d
developeruche <nweleuchenna2004@gmail.com>
2022-11-23 08:55:49 +0100

    developeruche: done working on the access control library

---
+++ contracts/facets/AccessControl.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+
+/// @notice this contract would be handling access control matter for the web3bridge doa ecosystem
+contract AccessControl {
+
+}
