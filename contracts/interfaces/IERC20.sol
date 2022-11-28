1ece2c75e5ad2e4c912e2084f1a59b1b4f00da2d
kayzee <olaniyikazeem112@gmail.com>
2022-11-28 20:46:22 +0100

    .

--- contracts/interfaces/IERC20.sol
+++ contracts/interfaces/IERC20.sol
@@ -16,7 +16,7 @@ interface IERC20 {
     function allowance(address owner, address spender) external view returns (uint256);
 
     function approve(address spender, uint256 amount) external returns (bool);
- 
+
     function transferFrom(address from,address to,uint256 amount) external returns (bool);
-    
+
 }
