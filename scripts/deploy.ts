c7bb4f637e0c3720a24debf3abcc115bf4ac02dd
developeruche <nweleuchenna2004@gmail.com>
2022-12-16 01:55:31 +0100

    developeruche: added the access control facet to the diamond cut

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -20,19 +20,19 @@ export async function deployDiamond() {
   const DiamondCutFacet = await ethers.getContractFactory("DiamondCutFacet");
   const diamondCutFacet = await DiamondCutFacet.deploy();
   await diamondCutFacet.deployed();
-  console.log("DiamondCutFacet deployed:", diamondCutFacet.address);
+  // console.log("DiamondCutFacet deployed:", diamondCutFacet.address);  // commenting because of test pourpose 
 
   // deploying vault 10
   const Vault10 = await ethers.getContractFactory("Vault10");
   const vault10 = await Vault10.deploy(USDC_ADDRESS, contractOwner.address);
   await vault10.deployed();
-  console.log("10% vault has been deployed: ", vault10.address);
+  // console.log("10% vault has been deployed: ", vault10.address);
 
   //deploying vault 5
   const Vault5 = await ethers.getContractFactory("Vault5");
   const vault5 = await Vault5.deploy(USDC_ADDRESS, contractOwner.address);
   await vault5.deployed();
-  console.log("5% vault has been deployed: ", vault5.address);
+  // console.log("5% vault has been deployed: ", vault5.address);
 
 
 
@@ -40,13 +40,13 @@ export async function deployDiamond() {
   const PreCert = await ethers.getContractFactory("PreCertificateToken");
   const preCert = await PreCert.deploy(contractOwner.address, vault10.address, vault5.address, DAO_TRESURY);
   await preCert.deployed();
-  console.log("Deployed Pre Certificate token: ", preCert.address);
+  // console.log("Deployed Pre Certificate token: ", preCert.address);
 
   // deploy DAO token
   const DAOToken = await ethers.getContractFactory("DAOtoken");
   const _DAOToken = await DAOToken.deploy();
   await _DAOToken.deployed();
-  console.log("DAOToken deployed: ", _DAOToken.address);
+  // console.log("DAOToken deployed: ", _DAOToken.address);
 
   DAO_TOKEN = _DAOToken;
 
@@ -61,7 +61,7 @@ export async function deployDiamond() {
     preCert.address
   );
   await diamond.deployed();
-  console.log("Diamond deployed:", diamond.address);
+  // console.log("Diamond deployed:", diamond.address);
 
   // transferring ownership of DAOToken to the diamond 
   await _DAOToken.setDiamondAddress(diamond.address);
@@ -72,18 +72,18 @@ export async function deployDiamond() {
   const DiamondInit = await ethers.getContractFactory("DiamondInit");
   const diamondInit = await DiamondInit.deploy();
   await diamondInit.deployed();
-  console.log("DiamondInit deployed:", diamondInit.address);
+  // console.log("DiamondInit deployed:", diamondInit.address);
 
   // deploy facets
-  console.log("");
-  console.log("Deploying facets");
-  const FacetNames = ["DiamondLoupeFacet", "OwnershipFacet", "AdminOpsFacet", "CertificateFactoryFacet", "GovernanceFacet", "TipsTokenFactoryFacet"];
+  // console.log("");
+  // console.log("Deploying facets");
+  const FacetNames = ["DiamondLoupeFacet", "OwnershipFacet", "AdminOpsFacet", "CertificateFactoryFacet", "GovernanceFacet", "TipsTokenFactoryFacet", "AccessControl"];
   const cut = [];
   for (const FacetName of FacetNames) {
     const Facet = await ethers.getContractFactory(FacetName);
     const facet = await Facet.deploy();
     await facet.deployed();
-    console.log(`${FacetName} deployed: ${facet.address}`);
+    // console.log(`${FacetName} deployed: ${facet.address}`);
     cut.push({
       facetAddress: facet.address,
       action: FacetCutAction.Add,
@@ -92,8 +92,8 @@ export async function deployDiamond() {
   }
 
   // upgrade diamond with facets
-  console.log("");
-  console.log("Diamond Cut:", cut);
+  // console.log("");
+  // console.log("Diamond Cut:", cut);
   const diamondCut = (await ethers.getContractAt(
     "IDiamondCut",
     diamond.address
@@ -103,12 +103,16 @@ export async function deployDiamond() {
   // call to init function
   let functionCall = diamondInit.interface.encodeFunctionData("init");
   tx = await diamondCut.diamondCut(cut, diamondInit.address, functionCall);
-  console.log("Diamond cut tx: ", tx.hash);
+  // console.log("Diamond cut tx: ", tx.hash);
   receipt = await tx.wait();
   if (!receipt.status) {
     throw Error(`Diamond upgrade failed: ${tx.hash}`);
   }
-  console.log("Completed diamond cut");
+  // console.log("Completed diamond cut");
+
+
+  const AccessControl = await ethers.getContractAt("AccessControl", diamond.address);
+  AccessControl.setUp(contractOwner.address); // this would set the superuser of the system to first signer who is also the diamond owner
   DiamondAddress = diamond.address;
 }
 
