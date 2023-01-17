166c979e59ac82177b42d5e803307a4fec5b0d57
Adebara Ayomide <adebaraayomide.com>
2023-01-17 10:48:47 +0100

    Juliet: TipsToken tests with accesscontrol implemented

--- contracts/utils/TipsToken.sol
+++ contracts/utils/TipsToken.sol
@@ -3,25 +3,29 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
+import {IAccessControl, Dto} from "../interfaces/IAccessControl.sol";
+
 /// @dev this contract is the tips token contract which will be minted to cohort students
 /// which they use to tip mentors when they receive help from these mentors
 contract TipsToken is ERC20 {
     constructor(
         string memory _tokenName,
         string memory _tokenSymbol,
-        address _owner
+        address _diamond
     ) ERC20(_tokenName, _tokenSymbol) {
-        owner = _owner;
+       diamond = _diamond;
         tokenName = _tokenName;
         tokenSymbol = _tokenSymbol;
     }
 
     bytes32 public merkleRoot;
-    address owner;
+    address diamond;
     string tokenName;
     string tokenSymbol;
     uint216 NumOfTokensToMint;
     mapping(address => bool) public studentClaimed;
+    mapping(address => uint) public tipsSent;
+    mapping(address => uint) public tipsRceived; 
 
     function whitelistMint(bytes32[] calldata _merkleProof) internal view returns (bool status) {
         require(!studentClaimed[msg.sender], "Address already claimed");
@@ -38,12 +42,12 @@ contract TipsToken is ERC20 {
     }
 
     function changeHashRoot(bytes32 _merkleRoot) external {
-        assert(msg.sender == owner);
+          assert(IAccessControl(diamond).hasRole(Dto.Roles.TOKEN_FACTORY, msg.sender));
         merkleRoot = _merkleRoot;
     }
 
     function setNumberOfTokensToMint(uint216 _NumOfTokensTMint) external {
-        assert(msg.sender == owner);
+       assert(IAccessControl(diamond).hasRole(Dto.Roles.TOKEN_FACTORY, msg.sender));
         NumOfTokensToMint = _NumOfTokensTMint;
     }
 
@@ -62,4 +66,35 @@ contract TipsToken is ERC20 {
     function symbol() public view virtual override returns (string memory) {
         return tokenSymbol;
     }
+
+    function tipsTokenSent(address _addr) external view returns(uint sent) {
+        sent = tipsSent[_addr];
+    }
+
+    function tipsTokenRecieved (address _addr) external view returns(uint received) {
+        received = tipsRceived[_addr];
+    }
+
+    function transfer(address to, uint256 amount) public virtual override returns (bool) {
+        address owner = _msgSender();
+        _transfer(owner, to, amount);
+        tipsRceived[to] += amount;
+        tipsSent[owner] += amount;
+        return true;
+    }
+
+     function transferFrom(
+        address from,
+        address to,
+        uint256 amount
+    ) public virtual override returns (bool) {
+        address spender = _msgSender();
+        _spendAllowance(from, spender, amount);
+        _transfer(from, to, amount);
+        tipsRceived[to] += amount;
+        tipsSent[from] += amount;
+        return true;
+    }
+
+
 }
